13a866f6fe37a753e65e244d52f941d03e780f03
wojtek2kdev <wojtek2kdev@gmail.com>
2017-12-19 20:14:54 +0100

    First test client-server connection

---
+++ Tests/client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import socket
+
+class Client:
+
+	TCP_IP = '127.0.0.1'
+	TCP_PORT = 1979
+	BUFFER_SIZE = 1024
+	MESSAGE = "Hello, World!"
+	SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	def __init__(self):
+		self.TCP_IP = raw_input("ip (127.0.0.1 by default): ") or self.TCP_IP
+		port = raw_input('port (1979 by default): ')
+		self.TCP_PORT = int(port) if port else self.TCP_PORT
+
+	def sendData(self, data):
+		self.SOCKET.connect((self.TCP_IP, self.TCP_PORT))
+		self.SOCKET.send(data)
+		recv = self.SOCKET.recv(self.BUFFER_SIZE)
+		self.SOCKET.close()
+		return recv
+
+c = Client()
+c.sendData('test')
