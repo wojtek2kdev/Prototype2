331f8d3518a4816008a2dc316676cb9717bddc2a
wojtek2kdev <wojtek2kdev@gmail.com>
2017-12-19 19:16:33 +0100

    Run server with configuration file

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
+import json
+
+def getServerInfo():
+	try:
+		with open('config.json', 'r') as config:
+			return json.loads(config.read())
+	except Exception as e:
+		print e
+
+
 import socket
 
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 1979
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+config = getServerInfo()
+
+TCP_IP = config['ip']
+TCP_PORT = config['port']
+BUFFER_SIZE = config['buffer_size']  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
